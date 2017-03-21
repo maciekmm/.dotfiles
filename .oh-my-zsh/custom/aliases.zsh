500ff20ffde6609ece0b6f6b2771d1e30935cd9b
Maciej Mionskowski <business@maciekmm.net>
2017-03-21 20:05:56 +0100

    Make calendar start with Monday

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,5 +5,8 @@ alias pjson="ruby -r json -e 'jj JSON.parse gets'"
 alias df='df -h'
 alias du='du -h'
 
+# Monday as the first day of the week
+alias cal='cal -m'
+
 # Fix PACMAN
 alias fixit='sudo rm -f /var/lib/pacman/db.lck && sudo pacman-mirrors -g && sudo pacman -Syyuu  && sudo pacman -Suu'
